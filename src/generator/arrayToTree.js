885291659aa7449eed412dc5a3257a8b9e83f875
Maciej Krüger <mkg20001@gmail.com>
2019-11-20 22:17:11 +0100

    feat: open api

--- src/generator/arrayToTree.js
+++ src/generator/arrayToTree.js
@@ -1,17 +1,36 @@
 'use strict'
 
+function createTree (name) {
+  return {
+    child: {},
+    name
+  }
+}
+
+function createLeaf (method) {
+  return method // TODO: better structure
+}
+
 module.exports = (array) => {
-  const tree = {}
+  const tree = createTree('/')
 
   array.forEach(path => {
-    const t = tree
-    const c = tree['/']
+    let c = tree
 
     path.elements.forEach(el => {
       if (el.param) {
-
+        if (!c.params) {
+          c.dynamic = true
+          c.params.push(el)
+        }
+      } else if (el.sub) {
+        c = c.child[el.name] = createTree(el.name)
       }
     })
+
+    for (const method in path.methods) { // eslint-disable-line guard-for-in
+      c.child[method] = createLeaf(path.methods[method])
+    }
   })
 
   return tree
--- src/generator/pathsToArray.js
+++ src/generator/pathsToArray.js
@@ -16,6 +16,33 @@ module.exports = (paths) => {
 
     const s = path.split('/')
 
+    const params = {}
+
+    ;(src.parameters || []).forEach(param => { // TODO: add via joi validation?
+      if (!params[param.in]) params[param.in] = {}
+      params[param.in][param.name] = param
+    })
+
+    s.forEach(el => {
+      let m
+      if ((m = el.match(/^\{(.+)\}$/))) {
+        const { name, description, type, required } = params.path[m[1]]
+
+        o.elements.push({
+          param: true,
+          name,
+          description,
+          type,
+          required
+        })
+      } else {
+        o.elements.push({
+          sub: true,
+          name: el
+        })
+      }
+    })
+
     METHODS.forEach(m => {
       if (src[m]) {
         o.methods[m] = src[m]
